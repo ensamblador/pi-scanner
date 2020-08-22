e1be130c646054d787f796894496a04d5b6ba4cb
Enrique Rodriguez <ensamblador@MacBook-Pro-de-Enrique.local>
2020-08-22 14:02:19 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from aws_cdk import core
+
+from portal_inmobiliario_scanner.portal_inmobiliario_scanner_stack import PortalInmobiliarioScannerStack
+
+
+app = core.App()
+PortalInmobiliarioScannerStack(app, "portal-inmobiliario-scanner")
+
+app.synth()
